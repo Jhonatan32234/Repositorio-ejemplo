296e88514372048fe9f6d09cace20c6e2c10b7cd
Jhonatan32234 <152237788+Jhonatan32234@users.noreply.github.com>
2024-09-10 11:39:34 -0600

    Add files via upload

---
+++ 1a/src/index.js
@@ -0,0 +1,13 @@
+import{setArr ,getArr, burbuja}from "./burbuja.js"
+
+const gen = document.getElementById("gen");
+gen.addEventListener("click",function(){
+    setArr();
+    const p = document.getElementById("out")
+p.innerText = getArr();
+})
+const b = document.getElementById("btn");
+b.addEventListener("click",function(){
+    const pOut= document.getElementById("outO");
+    pOut.innerText = burbuja();
+})
